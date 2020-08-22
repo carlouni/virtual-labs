76a9c782e14a94a8774c44f8627977c177b5ccb0
Gonzalo Carlo Cardenas Magno <gcmuni@gmail.com>
2020-08-23 08:18:47 +1000

    feature(wp plugin) definition of WP plugin

---
+++ virtual-labs/virtual-labs.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Virtual Labs
+ * Plugin URI:        https://github.com/carlouni/virtual-labs
+ * Description:       Displays a list of APNIC virtual labs.
+ * Version:           0.0.1
+ * Requires at least: 5.5
+ * Requires PHP:      7.2
+ * Author:            Gonzalo Carlo Cardenas Magno
+ * Author URI:        https://github.com/carlouni
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ */
+
+ add_shortcode('virtual_labs', function() {
+    return "Hello World";
+ });
