7e0a7f6405c104ec1eb1121168962664b4e60b5c
Gonzalo Carlo Cardenas Magno <gcmuni@gmail.com>
2020-08-23 15:40:50 +1000

    webpack config code format

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
-    },
-    output: {
-        filename: "./assets/js/main.js",
-        path: path.resolve(__dirname, 'virtual-labs'),
-    }
-  };
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  output: {
+    filename: "./assets/js/main.js",
+    path: path.resolve(__dirname, "virtual-labs"),
+  },
+};
