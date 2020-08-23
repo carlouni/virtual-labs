0061ea813b829d4651db9ff6cd991f6499e3628a
Gonzalo Carlo Cardenas Magno <gcmuni@gmail.com>
2020-08-23 13:05:11 +1000

    feature(react app) Break full list into pages and display first page

--- src/components/VirtualLabs.js
+++ src/components/VirtualLabs.js
@@ -2,39 +2,48 @@ import React, { Component } from "react";
 import { getVirtualLabs } from "../services/virtualLabs";
 import VirtualLab from "./VirtualLab";
 import { ReactSortable } from "react-sortablejs";
+import chunck from "lodash/chunk";
 
 export default class VirtualLabs extends Component {
   constructor() {
     super();
     this.state = {
-      labs: [],
+      pages: [],
+      page: 0,
     };
-    this.onSort.bind(this);
   }
 
   async componentDidMount() {
     const labs = await getVirtualLabs();
+    const pages = chunck(labs, 5);
     this.setState({
-      labs: labs,
+        pages: pages,
     });
   }
 
-  onSort(sortedLabs) {
-    this.setState({ labs: sortedLabs });
-  }
-
   render() {
-    return (
-      <ReactSortable
-        list={this.state.labs}
-        setList={(sortedLabs) => {
-            this.setState({ labs: sortedLabs });
-        }}
-      >
-        {this.state.labs.map((lab) => (
-          <VirtualLab key={lab.id} data={lab}></VirtualLab>
-        ))}
-      </ReactSortable>
-    );
+    const { pages, page } = this.state;
+    if (pages.length > 0) {
+        return (
+            <ReactSortable
+              list={pages[0]}
+              setList={(sortedLabs) => {
+                  console.log(sortedLabs);
+                  const newPages = pages;
+                  newPages[page] = sortedLabs;
+                  this.setState({ pages: newPages });
+              }}
+            >
+              {pages[page].map((lab) => (
+                <VirtualLab key={lab.id} data={lab}></VirtualLab>
+              ))}
+            </ReactSortable>
+          );
+    } else {
+        return (
+            <p>Loading</p>
+        );
+    }
+
   }
 }
